2c74fb85ae040fccebee3205b1576091061a93d2
srilekha1818 <srilekhakavuri18@gmail.com>
2023-05-28 15:19:17 +0530

    committed

--- Arrays-DSA/MergingInterval.java
+++ Arrays-DSA/MergingInterval.java
@@ -29,12 +29,15 @@ public class MergingInterval {
     public static void main(String[] args){
         int[][] intervals={{1,3},{2,5},{8,13}};
         int[][] x=merge(intervals);
-        System.out.println("merged array is: "+x);
+        for(int i=0;i<x.length;i++){
+            for(int j=0;j<x[i].length;j++){
+                System.out.print(x[i][j]+" "); 
+            }
+            System.out.println();
+        }
+
+        }
 
     }
 
-    
-    }
-
-   
 
