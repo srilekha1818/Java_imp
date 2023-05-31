9d532554b12ec20e0921f76d81de9744ab26d0d2
srilekha1818 <srilekhakavuri18@gmail.com>
2023-05-31 14:20:44 +0530

    commit

--- Arrays-DSA/ConsecutiveOne.java
+++ Arrays-DSA/ConsecutiveOne.java
@@ -1,28 +1,24 @@
 public class ConsecutiveOne {
-    public static  int findMaxConsecutiveOnes(int[] nums) {
-        int maxOne=0;
-        int cnt=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==1)
-            {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxOne = 0;
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
                 cnt++;
-               
-                
-               
-            }
-            else if(nums[i]==0)
-            {
-                maxOne=Math.max(maxOne,cnt);
-                cnt=0;
+
+            } else if (nums[i] == 0) {
+                maxOne = Math.max(maxOne, cnt);
+                cnt = 0;
             }
         }
-        return Math.max(maxOne,cnt);
-        
+        return Math.max(maxOne, cnt);
+
     }
-    public static void main(String[] args){
-        int[] nums={1,1,1,0,1,1,0};
-       System.out.println("maximum number of consecutive 1's in the given binary array are: "+findMaxConsecutiveOnes(nums));
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 0, 1, 1, 0 };
+        System.out.println(
+                "maximum number of consecutive 1's in the given binary array are: " + findMaxConsecutiveOnes(nums));
 
     }
 }
